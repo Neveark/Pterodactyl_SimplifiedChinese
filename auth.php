bf92ff9df9dbcb0dd022be84ea90f47908046882
Neveark <alprenx@gmail.com>
2018-06-26 18:21:29 +0800

    提交至GitHub
    提交初步翻译。

---
+++ auth.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+    'not_authorized' => '你没有被授权执行这个操作.',
+    'auth_error' => '在登录时发生错误.',
+    'authentication_required' => '必须要进行验证才能继续.',
+    'remember_me' => '记住我',
+    'sign_in' => '登录',
+    'forgot_password' => '我忘记了我的密码!',
+    'request_reset_text' => '忘记了你的密码?请输入你的邮箱.',
+    'reset_password_text' => '重置你的账户密码.',
+    'reset_password' => '重置账户密码',
+    'email_sent' => '一封包含下一步说明的电子邮件已经被发送到了你的邮箱.',
+    'failed' => '提供的凭据不符合我们的记录,或者两步验证密钥出现问题.',
+    'throttle' => '错误次数太多,请在 :seconds 秒后重试.',
+    'password_requirements' => '密码必须包含一个大写字母,小写字母和一个数字,且必须要大于8位数.',
+    'request_reset' => '查找账户',
+    '2fa_required' => '两步验证',
+    '2fa_failed' => '提供的两步验证密钥错误.',
+    'totp_failed' => '在验证 TOTP 时发生了错误.',
+    '2fa_must_be_enabled' => '管理员要求必须启用两步验证才能使用面板.',
+];
